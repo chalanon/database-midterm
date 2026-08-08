cf2d2eb44dd27738b54e770d53490724ef3d0e2e
chalanon <nonkub2546@gmail.com>
2026-08-08 17:15:47 +0700

    Suppress hydration warning caused by browser extensions (e.g. Google Tag Assistant) injecting attributes into <html>

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="th">
+    <html lang="th" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
